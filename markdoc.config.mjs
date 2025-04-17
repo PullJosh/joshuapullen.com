e7faf6d2e3e9b504dbd636b0a036ab6b0615bfaa
Josh Pullen <JoshPullen27@gmail.com>
2025-04-17 16:48:51 -0400

    Add post: "the shuffle buddy manifesto"

--- markdoc.config.mjs
+++ markdoc.config.mjs
@@ -6,5 +6,26 @@ export default defineMarkdocConfig({
       render: component("./src/components/InlineEmailSignupForm.astro"),
       attributes: {},
     },
+    youtube: {
+      render: component("./src/components/YoutubeEmbed.astro"),
+      attributes: {
+        videoId: {
+          type: String,
+          required: true,
+        },
+        autoplay: {
+          type: Boolean,
+          default: false,
+        },
+        loop: {
+          type: Boolean,
+          default: false,
+        },
+        aspectRatio: {
+          type: String,
+          default: "16 / 9",
+        },
+      },
+    },
   },
 });
